2d9248fc328db39d541a2d34eaa5daa332a0f861
matthieu.bandelie <matbandeli@gmail.com>
2016-10-26 17:50:00 +0200

    frontend - installation correcte de foundation

--- RebelLegion/resources/views/header.blade.php
+++ RebelLegion/resources/views/header.blade.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Foundation Starter Template</title>
 		<link rel="stylesheet" href="css/foundation-icons.css" />
-		<link rel="stylesheet" href="css/foundation.css" />		
+		<link rel="stylesheet" href="css/app.css" />		
 		<link rel="stylesheet" href="css/custom.css" />
 		<script src="js/jquery.js"></script>
 			
