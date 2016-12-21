7b214ec60e88d26adf982883181414b0d2469568
Fábio Marques <Fbio Marques>
2016-12-21 12:09:13 +0100

    add adimin user on seeder

--- RebelLegion/database/seeds/UsersTableSeeder.php
+++ RebelLegion/database/seeds/UsersTableSeeder.php
@@ -12,5 +12,22 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
       factory(App\User::class, 100)->create();
+
+      $usr =[
+        'userName' => 'fabioman',
+        'firstName' => 'fabio',
+        'lastName' => 'marques',
+        'email' => 'fabioman.damotama@gmail.com',
+        'phoneNumber' => '123456',
+        'facebookURL' => 'http://www.google.com',
+        'isPersonalDataVisiblle' => '1',
+        'isAdmin' => '1',
+        'position' => 'officier',
+        'status' => 'actif',
+        'internationalRebelLegionURL' => 'http://www.google.com',
+        'avatarURL' => null,
+        'password' => bcrypt('123456')
+      ];
+      DB::table('users')->insert($usr);
     }
 }
